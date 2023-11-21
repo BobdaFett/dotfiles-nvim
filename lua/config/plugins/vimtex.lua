b3924366b7eaddeb9b759d9f4549f31cb5643698
Lucas Vas <57099895+BobdaFett@users.noreply.github.com>
2023-11-21 00:40:24 -0500

    Update vimtex.lua

--- lua/config/plugins/vimtex.lua
+++ lua/config/plugins/vimtex.lua
@@ -4,9 +4,7 @@ return {
         lazy = false,
         init = function()
             vim.g.vimtex_compiler_method = "latexmk"
-            vim.g.vimtex_view_method = "C:\\Users\\lvas4\\Appdata\\Local\\sumatrapdf.exe --reuse-instance --forward-search @tex @line @pdf"
-
-
+            
             -- setup keybind for vimtex autocompilation.
             vim.keymap.set("n", "<leader>vc", "<cmd>VimtexCompile<CR>")
         end
