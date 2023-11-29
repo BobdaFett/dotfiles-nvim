517401099ec3d28881167def10350192dc4fb462
BobdaFett <lvas463@gmail.com>
2023-11-29 00:58:43 -0500

    Fixed/commented configuration for OmniSharp-vim

--- lua/config/plugins/lsp/omni-sharp.lua
+++ lua/config/plugins/lsp/omni-sharp.lua
@@ -1,5 +1,7 @@
 return {
 	{
 		"OmniSharp/omnisharp-vim",
+		-- Make sure to run :OmniSharpInstall to install the server
+		-- Assuming that the lsp does not run, use :OmniSharpOpenLog to see the logs
 	},
 }
