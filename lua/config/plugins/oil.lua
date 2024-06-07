c68f37d317fa7c27244a1662e7cc801d5d27ebbf
blast <lvas463@gmail.com>
2024-06-07 08:24:59 -0400

    Replaced nvim-tree with oil.nvim

---
+++ lua/config/plugins/oil.lua
@@ -0,0 +1,54 @@
+-- return {
+-- 	"nvim-tree/nvim-tree.lua",
+-- 	dependencies = {
+-- 		"nvim-tree/nvim-web-devicons",
+-- 	},
+-- 	config = function()
+-- 		local nvimtree = require("nvim-tree")
+--
+-- 		-- Recommended settings from the documentation.
+-- 		vim.g.loaded_netrw = 1
+-- 		vim.g.loaded_netrwPlugin = 1
+--
+-- 		-- Configure nvim-tree
+-- 		nvimtree.setup({
+-- 			view = {
+-- 				width = 50,
+-- 			},
+-- 			renderer = {
+-- 				indent_markers = {
+-- 					enable = true,
+-- 				},
+-- 			},
+-- 			filters = {
+--         enable = true,
+--         dotfiles = false,
+--         git_ignored = false,
+--       },
+--       git = {
+--         enable = true,
+--       }
+-- 		})
+--
+-- 		-- Configure keybinds.
+-- 		local keymap = vim.keymap
+--
+-- 		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>") -- Toggle the file explorer.
+-- 		keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFile<CR>") -- Toggle file finder.
+-- 		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>") -- Collapses the file explorer.
+-- 		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>") -- Refreshes the file explorer.
+-- 	end,
+-- }
+
+return {
+  "stevearc/oil.nvim",
+  opts = {},
+  dependencies = {
+    "nvim-tree/nvim-web-devicons"
+  },
+  config = function()
+    require('oil').setup()
+
+    vim.keymap.set("n", "-", ":Oil<CR>", {desc = "Open parent directory"})
+  end
+}
